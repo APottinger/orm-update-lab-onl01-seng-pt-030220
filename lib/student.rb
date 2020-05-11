1732ee9e3ee1294939c68849bb380da61ddb4698
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-05-11 17:31:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql)
+    result = DB[:conn].execute(sql, name)
   end
 
 end
