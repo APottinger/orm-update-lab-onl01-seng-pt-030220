4606bf6dea112bd58c3b540370bc9a69d7694fcb
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-05-11 17:40:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,12 +52,13 @@ class Student
     if self.id 
       self.update
     else
-    sql = <<-SQL
-    INSERT INTO cats(name, grade)
-    VALUES(?,?)
-  SQL
-  DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      sql = <<-SQL
+      INSERT INTO cats(name, grade)
+      VALUES(?,?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end 
   end
   
   def update
