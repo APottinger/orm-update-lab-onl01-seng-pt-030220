54819b999b14510632444fe7016491b6fc1878ee
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-05-11 16:43:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,27 @@
 require_relative "../config/environment.rb"
 
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE IF NOT EXISTS students (
+        id PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+    
+  end 
+  attr_accessor :name, :grade, :id 
+  
+  def initialize(name:, grade:, id: nil)
+    @name, @grade, @id = name, grade, id 
+  end 
+  
+  def save 
+    
+  end 
 
 
 end
