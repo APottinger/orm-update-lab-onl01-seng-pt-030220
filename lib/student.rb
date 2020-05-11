19f5a67185fbb94d472ec1b47a80992709262ca7
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-05-11 17:47:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
   attr_accessor :name, :grade, :id 
   
   
-  def initialize(name, grade)
+  def initialize(name, grade, id = nil)
     @name, @grade, @id = name, grade, id 
   end 
   
